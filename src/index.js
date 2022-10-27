e81d4b36b87c63c94cc33801cfbb0f91970b8940
vehidtr <vehid.trtak@gmail.com>
2022-10-27 13:27:54 +0200

    Add Wallet Context

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Buffer } from 'buffer';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Buffer } from "buffer";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { WalletContextProvider } from "./contexts/WalletContext";
 
 window.Buffer = Buffer;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <WalletContextProvider>
+      <App />
+    </WalletContextProvider>
   </React.StrictMode>
 );
 
