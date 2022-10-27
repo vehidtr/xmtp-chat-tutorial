7e2ea197d9516fa93f75b5e866734cd06bf51cdc
vehidtr <vehid.trtak@gmail.com>
2022-10-27 13:57:03 +0200

    Add send gm message button

--- src/App.js
+++ src/App.js
@@ -6,7 +6,20 @@ import { XmtpContext } from "./contexts/XmtpContext";
 function App() {
   const { connectWallet, walletAddress, signer } = useContext(WalletContext);
   const [providerState] = useContext(XmtpContext);
-  
+  const { client } = providerState;
+
+  const sendMessage = async () => {
+    const message = "gm";
+    if (!client || !walletAddress) {
+      return;
+    }
+    const conversation = await client.conversations.newConversation(
+      walletAddress
+    );
+    if (!conversation) return;
+    await conversation.send(message);
+  };
+
   return (
     <div className="App">
       {walletAddress ? (
@@ -19,7 +32,12 @@ function App() {
               Connect to XMTP
             </button>
           ) : (
-            <p>Connected: {walletAddress}</p>
+            <>
+              <p>Connected: {walletAddress}</p>
+              <button className="btn" onClick={sendMessage}>
+                Send gm
+              </button>
+            </>
           )}
         </div>
       ) : (
